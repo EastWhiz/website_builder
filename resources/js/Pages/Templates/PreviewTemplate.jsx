4e2cfcae7bb0e4aa941d876648938ed4785cdad3
Abdullah Ayaz <atillawall@gmail.com>
2025-03-07 21:50:26 +0500

    popover is added now

--- resources/js/Pages/Templates/PreviewTemplate.jsx
+++ resources/js/Pages/Templates/PreviewTemplate.jsx
@@ -1,4 +1,7 @@
-import { Head, usePage } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
 import { useEffect, useState } from "react";
 
 export default function Dashboard({ id }) {
@@ -6,6 +9,20 @@ export default function Dashboard({ id }) {
     const [data, setData] = useState(false);
     const [mainHTML, setMainHTML] = useState('');
     const [mainCSS, setMainCSS] = useState('');
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        console.log(event);
+        if (!event.target.className.includes("MuiModal-backdrop")) {
+            if (!event.target.outerHTML.includes("closeCheck")) {
+                setAnchorEl(event.target);
+            }
+        }
+    };
+
+    const open = Boolean(anchorEl);
+    const popoverId = open ? 'simple-popover' : undefined;
 
     useEffect(() => {
 
@@ -43,36 +60,46 @@ export default function Dashboard({ id }) {
 
         getData()
 
-        // const button = document.createElement("button");
-        // button.textContent = "Click Me";
-        // button.style.display = "block";
-        // button.style.marginTop = "10px"; // Optional: Adds spacing between paragraph and button
-
-        // document.addEventListener("click", function (event) {
-        //     console.log("You clicked somewhere on the page!", event.target);
-        //     // event.target.insertAdjacentElement("afterend", button);
-        //     // event.target.parentNode.insertBefore(button, event.target);
-        //     // event.target.insertAdjacentElement("afterbegin", button);
-
-        //     // Create a container div with flex styling
-        //     const container = document.createElement("div");
-        //     container.style.display = "flex";
-        //     container.style.alignItems = "center";
-
-        //     // Move the paragraph into the container
-
-        //     container.appendChild(event.target.cloneNode(true));
-        //     container.appendChild(button);
-
-        //     // Replace the original paragraph with the container
-        //     event.target.replaceWith(container);
-
-        // });
+        const button = document.createElement("button");
+        button.textContent = "Click Me";
+        button.style.display = "block";
+        button.style.marginTop = "10px"; // Optional: Adds spacing between paragraph and button
+        document.addEventListener("click", function (event) {
+            event.preventDefault();
+            handleClick(event);
+            // console.log("You clicked somewhere on the page!", event.target.outerHTML);
+            // event.target.insertAdjacentElement("afterend", button);
+            // event.target.parentNode.insertBefore(button, event.target);
+            // event.target.insertAdjacentElement("afterbegin", button);
+            // Create a container div with flex styling
+            // const container = document.createElement("div");
+            // container.style.display = "flex";
+            // container.style.alignItems = "center";
+            // // Move the paragraph into the container
+            // container.appendChild(event.target.cloneNode(true));
+            // container.appendChild(button);
+            // // Replace the original paragraph with the container
+            // event.target.replaceWith(container);
+        });
     }, [])
 
     return (
 
         <div>
+            <Popover
+                id={popoverId}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={() => { }}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                <Typography sx={{ p: 2 }}>The content of the Popover.
+                    <div className='closeCheck' onClick={() => setAnchorEl(false)}>❌</div>
+                </Typography>
+            </Popover>
             <Head title={`Preview (${data && data.template.name})`} />
             <div>
                 {data &&
