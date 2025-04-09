46f380fed81ad6c4f092f05dc1971c5233b40c2f
Abdullah Ayaz <atillawall@gmail.com>
2025-04-10 02:22:45 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    // server: {
-    //     host: '127.0.0.1', // or use 'localhost'
-    //     port: 5173,        // you can change this if needed
-    //     hmr: {
-    //         host: 'localhost', // for HMR to work correctly on local
-    //     },
-    // },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
