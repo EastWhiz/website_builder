bff212546d7dc4f57f649aa3d99d8e42a2326066
Abdullah Ayaz <atillawall@gmail.com>
2025-05-23 22:40:43 +0500

    not needed code commented

--- routes/web.php
+++ routes/web.php
@@ -109,23 +109,24 @@ Route::middleware(['auth', 'verified'])->group(function () {
     });
 });
 
-Route::get('/templates/preview/{id}', function ($id) {
-    return Inertia::render('Templates/PreviewTemplate', compact('id'));
-})->name('previewTemplate');
-Route::post('/templates/preview/contents', function (Request $request) {
-    $thisTemplate = Template::find($request->template_id);
-    return response()->json([
-        'message' => 'Data retrieved successfully',
-        'data' => [
-            'template' => $thisTemplate,
-            'css' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'css')->first(),
-            'js' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'js')->first(),
-            'body' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'html')->where('name', "BD1")->first(),
-            'body2' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'html')->where('name', "BD2")->first(),
-            'body3' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'html')->where('name', "BD3")->first(),
-        ],
-        'status' => 200
-    ]);
-})->name('templates.previewContent');
+// Route::get('/templates/preview/{id}', function ($id) {
+//     return Inertia::render('Templates/PreviewTemplate', compact('id'));
+// })->name('previewTemplate');
+
+// Route::post('/templates/preview/contents', function (Request $request) {
+//     $thisTemplate = Template::find($request->template_id);
+//     return response()->json([
+//         'message' => 'Data retrieved successfully',
+//         'data' => [
+//             'template' => $thisTemplate,
+//             'css' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'css')->first(),
+//             'js' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'js')->first(),
+//             'body' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'html')->where('name', "BD1")->first(),
+//             'body2' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'html')->where('name', "BD2")->first(),
+//             'body3' => TemplateContent::where('template_uuid', $thisTemplate->uuid)->where('type', 'html')->where('name', "BD3")->first(),
+//         ],
+//         'status' => 200
+//     ]);
+// })->name('templates.previewContent');
 
 require __DIR__ . '/auth.php';
