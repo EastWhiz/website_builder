38b4da680533ba2cbaf72784be096416d31e5b8d
Abdullah Ayaz <atillawall@gmail.com>
2025-06-03 06:15:57 +0500

    changes for admin password

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,6 +14,7 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::create([
+            'id' => 1,
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => 'Admin@321',
