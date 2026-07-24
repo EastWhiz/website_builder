655ad534a7f0e8c7363984d7dda1210c88a0c059
Mubeen <mubeen.devtech@gmail.com>
2026-07-24 19:49:25 +0500

    phase 0, 1 and 2

--- app/Models/AngleTemplate.php
+++ app/Models/AngleTemplate.php
@@ -9,7 +9,27 @@ class AngleTemplate extends Model
 {
     use SoftDeletes;
 
-    protected $fillable = ['uuid', 'angle_id', 'template_id', 'user_id', 'organization_id', 'name', 'main_html', 'main_css', 'main_js'];
+    public const CONTENT_MODE_LEGACY = 'legacy';
+
+    public const CONTENT_MODE_STRUCTURED_BD = 'structured_bd';
+
+    protected $fillable = [
+        'uuid',
+        'angle_id',
+        'template_id',
+        'user_id',
+        'organization_id',
+        'name',
+        'main_html',
+        'main_css',
+        'main_js',
+        'content_mode',
+        'structured_version',
+    ];
+
+    protected $casts = [
+        'structured_version' => 'integer',
+    ];
 
     public function angle()
     {
@@ -34,6 +54,21 @@ class AngleTemplate extends Model
     public function contents()
     {
         return $this->hasMany(ExtraContent::class, 'angle_template_uuid', 'uuid');
+    }
+
+    public function bdContents()
+    {
+        return $this->hasMany(AngleTemplateBdContent::class, 'angle_template_id');
+    }
+
+    public function isStructuredBd(): bool
+    {
+        return $this->content_mode === self::CONTENT_MODE_STRUCTURED_BD;
+    }
+
+    public function isLegacyContent(): bool
+    {
+        return ! $this->isStructuredBd();
     }
 
     public function scopeForOrganization($query, ?int $organizationId)
